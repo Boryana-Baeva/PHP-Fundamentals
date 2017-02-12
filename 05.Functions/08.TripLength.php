abaa0f41b65bf720c869bb813618e391dfeb7729
Boryana Baeva <borenceto@gmail.com>
2017-02-12 22:52:28 +0200

    Update 08.TripLength.php

--- 05.Functions/08.TripLength.php
+++ 05.Functions/08.TripLength.php
@@ -4,23 +4,9 @@ $input = explode(", ", trim(fgets(STDIN)));
 $input = array_map('floatval', $input);
 list($x1, $y1, $x2, $y2, $x3, $y3) = [$input[0], $input[1], $input[2], $input[3], $input[4], $input[5]];
 
-
 $distance12 = calculateDistance($x1, $y1, $x2, $y2);
 $distance13 = calculateDistance($x1, $y1, $x3, $y3);
 $distance23 = calculateDistance($x2, $y2, $x3, $y3);
-
-
-/*if (($distance12 <= $distance13) && ($distance13 >= $distance23)) {
-    $a = $distance12 + $distance23;
-    echo '1->2->3: ' . $a;
-
-} else if (($distance12 <= $distance23) && ($distance13 <= $distance23)) {
-    $b = $distance13 + $distance12;
-    echo '2->1->3: ' . $b;
-} else {
-    $c = $distance23 + $distance13;
-    echo '1->3->2: ' . $c;
-}*/
 
 if ($distance12 <= $distance13 && $distance12 <= $distance23) {
     if ($distance13 <= $distance23) {
