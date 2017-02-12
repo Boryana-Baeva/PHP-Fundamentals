5f7a41decbd708d1dc0f840ce2d1b319de53ee47
Boryana Baeva <borenceto@gmail.com>
2017-02-12 22:40:46 +0200

    Update 01.DefineClassPerson.php

--- 06.OOP Basics/01.DefineClassPerson.php
+++ 06.OOP Basics/01.DefineClassPerson.php
@@ -1,13 +1,5 @@
 <?php
 
-$person = new Person();
-echo(count(get_object_vars($person)));
-
-$person1 = new Person("Pesho", 20);
-/*$person2 = new Person("Gosho", 18);
-$person3 = new Person("Stamat", 43);*/
-
-echo $person1->getName();
 class Person
 {
     private $name = null;
@@ -30,5 +22,11 @@ class Person
     }
 }
 
+$person = new Person();
+echo(count(get_object_vars($person)));
 
+$person1 = new Person("Pesho", 20);
+/*$person2 = new Person("Gosho", 18);
+$person3 = new Person("Stamat", 43);*/
 
+echo $person1->getName();
