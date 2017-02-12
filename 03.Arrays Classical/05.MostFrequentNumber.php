215b69bcefa2aa776c789d300e115f0b09896908
Boryana Baeva <borenceto@gmail.com>
2017-02-12 22:55:35 +0200

    Update 05.MostFrequentNumber.php

--- 03.Arrays Classical/05.MostFrequentNumber.php
+++ 03.Arrays Classical/05.MostFrequentNumber.php
@@ -1,13 +1,4 @@
 <?php
-/*$input = array_map('trim', explode(' ', fgets(STDIN)));
-//$input = array_intersect($input, range(0, 65535));
-$occurrences = array_count_values($input);             // Некоректно поведение на array_count_values при отрицателни стойности
-$mostFrequent = strval(max($occurrences));
-
-echo array_search($mostFrequent, $occurrences);
-
-*/
-
 
 $inputNumbers = trim(fgets(STDIN));
 //$inputNumbers = "2 2 2 2 1 2 2 2";
