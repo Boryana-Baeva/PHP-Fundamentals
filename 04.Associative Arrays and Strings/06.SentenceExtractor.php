73239466b0d36afac380b8a9ef99959fadc3148b
Boryana Baeva <borenceto@gmail.com>
2017-02-12 22:54:39 +0200

    Update 06.SentenceExtractor.php

--- 04.Associative Arrays and Strings/06.SentenceExtractor.php
+++ 04.Associative Arrays and Strings/06.SentenceExtractor.php
@@ -6,16 +6,3 @@ $word = trim(fgets(STDIN));
 preg_match_all("/([^.?!]*\\b" . $word . "\\b[^.?!]*[.?!])/", $text, $result);
 $sentences = array_map('trim',$result[0]);
 echo implode("\n", $sentences);
-
-
-
-/*$text = preg_split("/[.!?]/", $text);
-
-/*foreach ($text as $sentence) {
-    if (preg_match("/(\\b" . $word . "\\b)/", $sentence)) {
-        echo "yes\n";
-    } else {
-        echo "no\n";
-    }
-}
-*/
