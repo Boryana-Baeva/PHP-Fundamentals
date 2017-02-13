15d1dd03d8743d03cb2fb181381dbbb660ca8be4
Boryana Baeva <borenceto@gmail.com>
2017-02-14 01:57:13 +0200

    Update 06.RawData.php

--- 06.OOP Basics/06.RawData.php
+++ 06.OOP Basics/06.RawData.php
@@ -1,12 +1,17 @@
 <?php
 
-class Car{
+class Car
+{
     private $model;
     private $engine;
     private $cargo;
     private $tyres = array();
 
-    function __construct(string $model, Engine $engine, Cargo $cargo, Tyre $tyre1, Tyre $tyre2, Tyre $tyre3, Tyre $tyre4)
+    function __construct(string $model,
+                         Engine $engine,
+                         Cargo $cargo,
+                         Tyre $tyre1, Tyre $tyre2,
+                         Tyre $tyre3, Tyre $tyre4)
     {
         $this->model = $model;
         $this->engine = $engine;
@@ -35,7 +40,8 @@ class Car{
     }
 }
 
-class Engine{
+class Engine
+{
     private $speed;
     private $power;
 
@@ -52,7 +58,8 @@ class Engine{
 
 }
 
-class Cargo{
+class Cargo
+{
     private $weight;
     private $type;
 
@@ -68,7 +75,8 @@ class Cargo{
     }
 }
 
-class Tyre{
+class Tyre
+{
     private $pressure;
     private $age;
 
@@ -86,16 +94,18 @@ class Tyre{
 
 $numberOfCars = intval(trim(fgets(STDIN)));
 $cars = [];
-for($i = 0; $i < $numberOfCars; $i++){
-    $carInfo = explode(" ",trim(fgets(STDIN)));
+for ($i = 0; $i < $numberOfCars; $i++) {
+    $carInfo = explode(" ", trim(fgets(STDIN)));
     list($model, $engineSpeed, $enginePower,
         $cargoWeight, $cargoType, $tyre1Pressure,
         $tyre1Age, $tyre2Pressure, $tyre2Age,
-        $tyre3Pressure,$tyre3Age, $tyre4Pressure, $tyre4Age) =
+        $tyre3Pressure, $tyre3Age,
+        $tyre4Pressure, $tyre4Age) =
         [$carInfo[0], intval($carInfo[1]), intval($carInfo[2]),
-            intval($carInfo[3]),$carInfo[4], floatval($carInfo[5]),
-             intval($carInfo[6]), floatval($carInfo[7]), intval($carInfo[8]),
-                floatval($carInfo[9]), intval($carInfo[10]), floatval($carInfo[11]), intval($carInfo[12])];
+            intval($carInfo[3]), $carInfo[4], floatval($carInfo[5]),
+            intval($carInfo[6]), floatval($carInfo[7]), intval($carInfo[8]),
+            floatval($carInfo[9]), intval($carInfo[10]),
+            floatval($carInfo[11]), intval($carInfo[12])];
 
     $engine = new Engine($engineSpeed, $enginePower);
     $cargo = new Cargo($cargoWeight, $cargoType);
@@ -104,27 +114,31 @@ for($i = 0; $i < $numberOfCars; $i++){
     $tyre3 = new Tyre($tyre3Pressure, $tyre3Age);
     $tyre4 = new Tyre($tyre4Pressure, $tyre4Age);
 
-    $car = new Car($model, $engine, $cargo, $tyre1, $tyre2, $tyre3, $tyre4);
+    $car = new Car($model, $engine, $cargo,
+        $tyre1, $tyre2, $tyre3, $tyre4);
     $cars[] = $car;
 }
 
 //print_r($cars);
 $type = trim(fgets(STDIN));
 $output = [];
-foreach ($cars as $car){
-    if($car->getCargo()->getType() == $type){
-        $tyres = $car->getTyres();
-        foreach ($tyres as $tyre){
-            if($tyre->getPressure() < 1){
-                $output[] = $car->getModel();
-                break;
-            }
-        }
-
-        if($car->getEngine()->getPower() > 250){
+foreach ($cars as $car) {
+    if ($car->getCargo()->getType() === $type) {
+        if ($type === 'flamable' && $car->getEngine()->getPower() > 250) {
             $output[] = $car->getModel();
+
+        } else if($type === 'fragile'){
+            $tyres = $car->getTyres();
+            foreach ($tyres as $tyre) {
+                if ($tyre->getPressure() < 1) {
+                    $output[] = $car->getModel();
+                    break;
+                }
+            }
         }
     }
 }
 
-echo implode(PHP_EOL, $output);
+foreach ($output as $outputCar) {
+    echo $outputCar . PHP_EOL;
+}
